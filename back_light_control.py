837362cb3667f2fb4feda3e34e9be3d6b0368ef7
Robert1991 <rwaddell91@googlemail.com>
2021-01-20 20:56:43 +0100

    added autodiscovery to backlight

--- back_light_control.py
+++ back_light_control.py
@@ -13,6 +13,7 @@ light_id = "aWOY6O"
 light_name = "touch_screen_back_light"
 command_topic = auto_configure_prefix + "/light/" + light_name + "/set"
 state_topic = auto_configure_prefix + "/light/" + light_name + "/state"
+homeassistant_status_topic = "homeassistant/status"
 
 auto_configure_json = {"unique_id": light_id,
                        "name": light_name,
@@ -32,13 +33,12 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.publish(auto_configure_topic, json.dumps(auto_configure_json))
     client.subscribe(command_topic)
-
+    client.subscribe(homeassistant_status_topic)
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     if msg.topic == command_topic:
         payload = str(msg.payload)
-        print(payload)
         if payload == "b'ON'":
             os.system(
                 "sudo sh -c 'echo \"0\" > /sys/class/backlight/rpi_backlight/bl_power'")
@@ -47,6 +47,8 @@ def on_message(client, userdata, msg):
             os.system(
                 "sudo sh -c 'echo \"1\" > /sys/class/backlight/rpi_backlight/bl_power'")
             client.publish(state_topic, "OFF")
+    elif msg.topic == "homeassistant/status":
+        client.publish(auto_configure_topic, json.dumps(auto_configure_json))
 
 
 homeassistant_broker = "homeassistant"
@@ -57,7 +59,6 @@ client = mqtt.Client()
 client.username_pw_set(mqtt_user, password=mqtt_passwd)
 client.on_connect = on_connect
 client.on_message = on_message
-
 
 client.connect(homeassistant_broker, 1883, 60)
 client.loop_forever()
